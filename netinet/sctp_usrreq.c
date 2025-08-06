a659ecd64c64e000e0e835a14b4c456e44c7e2e2
Michael Tuexen <tuexen@fh-muenster.de>
2025-08-06 17:50:03 +0200

    Updates from FreeBSD upstream

--- netinet/sctp_usrreq.c
+++ netinet/sctp_usrreq.c
@@ -604,8 +604,9 @@ sctp_getcred(SYSCTL_HANDLER_ARGS)
 	/* FIX, for non-bsd is this right? */
 	vrf_id = SCTP_DEFAULT_VRFID;
 
+	if (req->newptr == NULL)
+		return (EINVAL);
 	error = priv_check(req->td, PRIV_NETINET_GETCRED);
-
 	if (error)
 		return (error);
 
@@ -8891,7 +8892,6 @@ sctp_peeraddr(struct socket *so, struct mbuf *nam)
 	.pr_control =	in_control,				\
 	.pr_close =	sctp_close,				\
 	.pr_detach =	sctp_close,				\
-	.pr_sopoll =	sopoll_generic,				\
 	.pr_flush =	sctp_flush,				\
 	.pr_disconnect = sctp_disconnect,			\
 	.pr_listen =	sctp_listen,				\
